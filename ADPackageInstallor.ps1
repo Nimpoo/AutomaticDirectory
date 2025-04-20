0b35656081b72e4e435c1218b569bc7875c5740d
Nimpoo <marwan.ayoub.pro@gmail.com>
2025-04-20 20:26:55 +0200

    `ADPackageInstallor.ps1` script security update

--- ADPackageInstallor.ps1
+++ ADPackageInstallor.ps1
@@ -1,3 +1,38 @@
-Add-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
-Add-WindowsFeature -Name DNS -IncludeManagementTools -IncludeAllSubFeature
-Add-WindowsFeature -Name RSAT-AD-Tools -IncludeManagementTools -IncludeAllSubFeature
+# Installation de "Active Directory Domain Services"
+$ADDS = Get-WindowsFeature -Name AD-Domain-Services
+try {
+    if ($ADDS.Installed) {
+        Write-Output "Active Directory Domain Service est DEJA installe."
+    } else {
+        Add-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
+        Write-Output "Active Directory Domain Service est desormais installe."
+    }
+} catch {
+    Write-Output "Erreur lors de l'installation de AD DS : [$_]"
+}
+
+# Installation de "Domain Name Service"
+$DNS = Get-WindowsFeature -Name DNS
+try {
+    if ($DNS.Installed) {
+        Write-Output "DNS est DEJA installe."
+    } else {
+        Add-WindowsFeature -Name DNS -IncludeManagementTools -IncludeAllSubFeature
+        Write-Output "DNS est desormais installe."
+    }
+} catch {
+    Write-Output "Erreur lors de l'installation de DNS : [$_]"
+}
+
+# Installation de "RSAT-AD-Tools"
+$RSAT = Get-WindowsFeature -Name RSAT-AD-Tools
+try {
+    if ($RSAT.Installed) {
+        Write-Output "RSAT-AD-Tools est DEJA installe."
+    } else {
+        Add-WindowsFeature -Name RSAT-AD-Tools -IncludeManagementTools -IncludeAllSubFeature
+        Write-Output "RSAT-AD-Tools est desormais installe."
+    }
+} catch {
+    Write-Output "Erreur lors de l'installation de RSAT-AD-Tools : [$_]"
+}
