8b4da3a0aeceb1a8da0a6483831e8e97a32c2060
Nimpoo <marwan.ayoub.pro@gmail.com>
2025-04-27 16:22:41 +0200

    `ADPackageInstallor.ps1` more beauty

--- ADPackageInstallor.ps1
+++ ADPackageInstallor.ps1
@@ -2,37 +2,37 @@
 $ADDS = Get-WindowsFeature -Name AD-Domain-Services
 try {
     if ($ADDS.Installed) {
-        Write-Output "Active Directory Domain Service est DEJA installe."
+        Write-Host -ForegroundColor Blue "Active Directory Domain Service est DEJA installe."
     } else {
         Add-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
-        Write-Output "Active Directory Domain Service est desormais installe."
+        Write-Host -ForegroundColor Green "Active Directory Domain Service est desormais installe."
     }
 } catch {
-    Write-Output "Erreur lors de l'installation de AD DS : [$_]"
+    Write-Host -ForegroundColor Red "Erreur lors de l'installation de AD DS : [$_]"
 }
 
 # Installation de "Domain Name Service"
 $DNS = Get-WindowsFeature -Name DNS
 try {
     if ($DNS.Installed) {
-        Write-Output "DNS est DEJA installe."
+        Write-Host -ForegroundColor Blue "DNS est DEJA installe."
     } else {
         Add-WindowsFeature -Name DNS -IncludeManagementTools -IncludeAllSubFeature
-        Write-Output "DNS est desormais installe."
+        Write-Host -ForegroundColor Green "DNS est desormais installe."
     }
 } catch {
-    Write-Output "Erreur lors de l'installation de DNS : [$_]"
+    Write-Host -ForegroundColor Red "Erreur lors de l'installation de DNS : [$_]"
 }
 
 # Installation de "RSAT-AD-Tools"
 $RSAT = Get-WindowsFeature -Name RSAT-AD-Tools
 try {
     if ($RSAT.Installed) {
-        Write-Output "RSAT-AD-Tools est DEJA installe."
+        Write-Host -ForegroundColor Blue "RSAT-AD-Tools est DEJA installe."
     } else {
         Add-WindowsFeature -Name RSAT-AD-Tools -IncludeManagementTools -IncludeAllSubFeature
-        Write-Output "RSAT-AD-Tools est desormais installe."
+        Write-Host -ForegroundColor Green "RSAT-AD-Tools est desormais installe."
     }
 } catch {
-    Write-Output "Erreur lors de l'installation de RSAT-AD-Tools : [$_]"
+    Write-Host -ForegroundColor Red "Erreur lors de l'installation de RSAT-AD-Tools : [$_]"
 }
